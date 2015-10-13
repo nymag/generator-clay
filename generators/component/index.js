b548acfcfe6723c921aeb08fd301b7f1e97cc233
Nelson Pecora <nelson@yoshokatana.com>
2015-10-13 16:07:41 -0400

    check for site/component on initialization

--- generators/component/index.js
+++ generators/component/index.js
@@ -1,13 +1,35 @@
 'use strict';
 
-var generators = require('yeoman-generator');
+var generators = require('yeoman-generator'),
+  fs = require('fs'),
+  chalk = require('chalk'),
+  _ = require('lodash');
 
 module.exports = generators.NamedBase.extend({
   constructor: function () {
     generators.NamedBase.apply(this, arguments);
   },
 
-  method1: function () {
-    console.log('generating new component: ' + this.name);
+  initializing: {
+    checkComponent: function () {
+      var name = this.name,
+        hasComponentFolder = fs.existsSync(this.destinationPath('components', name)),
+        hasNpmComponent;
+
+      try {
+        let pkg = require(this.destinationPath('package.json')),
+          deps = pkg.dependencies;
+
+        hasNpmComponent = _.contains(Object.keys(deps), 'clay-' + name);
+      } catch (e) {
+        hasNpmComponent = false;
+      }
+
+      if (hasComponentFolder || hasNpmComponent) {
+        console.log(chalk.red('Component already exists!'));
+      } else {
+        console.log(chalk.blue('Generating new component: ' + name));
+      }
+    }
   }
 });
--- generators/site/index.js
+++ generators/site/index.js
@@ -1,13 +1,24 @@
 'use strict';
 
-var generators = require('yeoman-generator');
+var generators = require('yeoman-generator'),
+  fs = require('fs'),
+  chalk = require('chalk');
 
 module.exports = generators.NamedBase.extend({
   constructor: function () {
     generators.NamedBase.apply(this, arguments);
   },
 
-  method1: function () {
-    console.log('generating new site: ' + this.name);
+  initializing: {
+    checkSite: function () {
+      var name = this.name,
+        hasSiteFolder = fs.existsSync(this.destinationPath('sites', name));
+
+      if (hasSiteFolder) {
+        console.log(chalk.red('Site already exists!'));
+      } else {
+        console.log(chalk.blue('Generating new site: ' + name));
+      }
+    }
   }
 });
