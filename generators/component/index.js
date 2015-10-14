611bf9ecdea4052642e1b3a835a79517c49e5584
Nelson Pecora <nelson@yoshokatana.com>
2015-10-14 17:25:59 -0400

    prompt user for template language

--- generators/component/index.js
+++ generators/component/index.js
@@ -40,6 +40,46 @@ module.exports = generators.NamedBase.extend({
     }
   },
 
+  prompting: {
+    getTplExpension: function () {
+      var done = this.async();
+
+      this.prompt([{
+        // ask for the template language
+        type: 'list',
+        name: 'templateLang',
+        message: 'What templating language do you want to use?',
+        default: 'nunjucks',
+        choices: [{
+          name: 'Nunjucks',
+          value: 'nunjucks'
+        }, {
+          name: 'Jade',
+          value: 'jade'
+        }, {
+          name: 'Other (enter on next prompt)',
+          value: 'other'
+        }],
+        store: true // store their defaults for the next time they use this
+      }, {
+        // ask for custom template language if they answered 'other'
+        type: 'input',
+        name: 'customTemplateLang',
+        message: 'Please type the extension you want',
+        filter: function (input) {
+          return input.indexOf('.') === 0 ? input.replace('.', '') : input;
+          // remove dot if they type it, e.g. .ejs -> ejs
+        },
+        when: function (answers) {
+          return answers.templateLang === 'other';
+        }
+      }], function (answers) {
+        this.tplExpension = answers.templateLang !== 'other' ? answers.templateLang : answers.customTemplateLang;
+        done();
+      }.bind(this));
+    }
+  },
+
   writing: {
     createFolder: function () {
       var done = this.async(),
