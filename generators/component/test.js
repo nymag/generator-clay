eebcaccd09da7ffe3e0dc9f0d731653aae71026c
Nelson Pecora <nelson@yoshokatana.com>
2015-10-15 10:25:50 -0400

    more organized tests

--- generators/component/test.js
+++ generators/component/test.js
@@ -21,129 +21,101 @@ function runGenerator(options, done) {
 }
 
 describe('clay:component', function () {
-  before(function (done) {
-    runGenerator({}, done);
+  describe('defaults', function () {
+    before(function (done) {
+      runGenerator({}, done);
+    });
+
+    it('generates a component folder', function () {
+      assert.file(folder);
+    });
+
+    it('adds an all.css stylesheet', function () {
+      var file = path.join(folder, 'all.css');
+
+      assert.file(file);
+      assert.fileContent(file, /^\.foo \{/);
+    });
+
+    it('adds a print.css stylesheet', function () {
+      var file = path.join(folder, 'print.css');
+
+      assert.file(file);
+      assert.fileContent(file, /^\.foo \{/);
+    });
+
+    it('adds a template', function () {
+      var file = path.join(folder, 'template.nunjucks');
+
+      assert.file(file);
+      assert.fileContent(file, /class="foo"/);
+    });
   });
 
-  it('generates a component folder', function () {
-    assert.file(folder);
+  describe('--tag', function () {
+    before(function (done) {
+      runGenerator({ tag: 'aside' }, done);
+    });
+
+    it('allows tag option', function () {
+      var file = path.join(folder, 'template.nunjucks');
+
+      assert.file(file);
+      assert.fileContent(file, /^<aside/);
+    });
   });
 
-  it('adds an all.css stylesheet', function () {
-    var file = path.join(folder, 'all.css');
+  describe('--viewports', function () {
+    /**
+     * test a viewport
+     * @param {string} viewport
+     * @param {Function} done async callback
+     */
+    function testViewport(viewport, done) {
+      runGenerator({ viewports: viewport }, function () {
+        assert.file(path.join(folder, viewportsHash[viewport] + '.css'));
+        done();
+      });
+    }
 
-    assert.file(file);
-    assert.fileContent(file, /^\.foo \{/);
-  });
+    it('m', function (done) {
+      testViewport('m', done);
+    });
 
-  it('adds a print.css stylesheet', function () {
-    var file = path.join(folder, 'print.css');
+    it('mobile', function (done) {
+      testViewport('mobile', done);
+    });
 
-    assert.file(file);
-    assert.fileContent(file, /^\.foo \{/);
-  });
+    it('t', function (done) {
+      testViewport('t', done);
+    });
 
-  it('adds a template', function () {
-    var file = path.join(folder, 'template.nunjucks');
+    it('tablet', function (done) {
+      testViewport('tablet', done);
+    });
 
-    assert.file(file);
-    assert.fileContent(file, /class="foo"/);
-  });
-});
+    it('d', function (done) {
+      testViewport('d', done);
+    });
 
-// tags option
-describe('clay:component --tag aside', function () {
-  before(function (done) {
-    runGenerator({ tag: 'aside' }, done);
-  });
+    it('desktop', function (done) {
+      testViewport('desktop', done);
+    });
 
-  it('allows tag option', function () {
-    var file = path.join(folder, 'template.nunjucks');
+    it('arbitrary', function (done) {
+      runGenerator({ viewports: '0-200' }, function () {
+        assert.file(path.join(folder, '0-200.css'));
+        done();
+      });
+    });
 
-    assert.file(file);
-    assert.fileContent(file, /^<aside/);
-  });
-});
-
-// viewports options
-describe('clay:component --viewports m', function () {
-  before(function (done) {
-    runGenerator({ viewports: 'm' }, done);
-  });
-
-  it('creates mobile viewport', function () {
-    assert.file(path.join(folder, viewportsHash.m + '.css'));
-  });
-});
-
-describe('clay:component --viewports mobile', function () {
-  before(function (done) {
-    runGenerator({ viewports: 'mobile' }, done);
-  });
-
-  it('creates mobile viewport', function () {
-    assert.file(path.join(folder, viewportsHash.mobile + '.css'));
-  });
-});
-
-describe('clay:component --viewports t', function () {
-  before(function (done) {
-    runGenerator({ viewports: 't' }, done);
-  });
-
-  it('creates tablet viewport', function () {
-    assert.file(path.join(folder, viewportsHash.t + '.css'));
-  });
-});
-
-describe('clay:component --viewports tablet', function () {
-  before(function (done) {
-    runGenerator({ viewports: 'tablet' }, done);
-  });
-
-  it('creates tablet viewport', function () {
-    assert.file(path.join(folder, viewportsHash.tablet + '.css'));
-  });
-});
-
-describe('clay:component --viewports d', function () {
-  before(function (done) {
-    runGenerator({ viewports: 'd' }, done);
-  });
-
-  it('creates desktop viewport', function () {
-    assert.file(path.join(folder, viewportsHash.d + '.css'));
-  });
-});
-
-describe('clay:component --viewports desktop', function () {
-  before(function (done) {
-    runGenerator({ viewports: 'desktop' }, done);
-  });
-
-  it('creates desktop viewport', function () {
-    assert.file(path.join(folder, viewportsHash.desktop + '.css'));
-  });
-});
-
-describe('clay:component --viewports 0-200', function () {
-  before(function (done) {
-    runGenerator({ viewports: '0-200' }, done);
-  });
-
-  it('creates arbitrary viewport', function () {
-    assert.file(path.join(folder, '0-200.css'));
-  });
-});
-
-describe('clay:component --viewports 0-300,300-600,600+', function () {
-  before(function (done) {
-    runGenerator({ viewports: '0-300,300-600,600+' }, done);
-  });
-
-  it('creates arbitrary viewports', function () {
-    assert.file(path.join(folder, '0-300.css'));
-    assert.file(path.join(folder, '300-600.css'));
-    assert.file(path.join(folder, '600+.css'));
+    it('arbitrary, multiple', function (done) {
+      runGenerator({ viewports: '0-300,300-600,600+' }, function () {
+        assert.file(path.join(folder, '0-300.css'));
+        assert.file(path.join(folder, '300-600.css'));
+        assert.file(path.join(folder, '600+.css'));
+        done();
+      });
+    });
   });
 });
