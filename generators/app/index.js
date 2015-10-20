bbd10ac1c51b0506856c222452281bb712f05159
techmsi <Sophie.Idromenos@nymag.com>
2015-10-20 11:25:00 -0400

    App: Package - Added appname

--- generators/app/index.js
+++ generators/app/index.js
@@ -7,13 +7,20 @@ module.exports = generators.Base.extend({
   constructor: function () {
     generators.Base.apply(this, arguments);
     this.log('Welcome to the ' + chalk.yellow.bold('Clay Instance') + ' generator!');
+
+    // Store user-inputed appname
+    this.argument('appname', { type: String, required: false });
+    this.appname = this.app_name || this.appname;
   },
 
   writing: {
     app: function () {
-      this.fs.copy(
+      this.fs.copyTpl(
         this.templatePath('_package.json'),
-        this.destinationPath('package.json')
+        this.destinationPath('package.json'),
+        {
+          appname: this.appname
+        }
       );
       this.fs.copy(
         this.templatePath('_app.js'),
@@ -21,11 +28,14 @@ module.exports = generators.Base.extend({
       );
     },
 
-    projectfiles: function () {
+    git: function () {
       this.fs.copy(
         this.templatePath('gitignore'),
         this.destinationPath('.gitignore')
       );
+    },
+
+    projectfiles: function () {
       this.fs.copy(
         this.templatePath('eslintrc'),
         this.destinationPath('.eslintrc')
@@ -40,5 +50,6 @@ module.exports = generators.Base.extend({
   // Run npm install
   install: function () {
     this.npmInstall();
+    this.log('Your app ' + chalk.yellow.bold(this.appname) + ' was created.');
   }
 });
