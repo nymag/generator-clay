51e6b20a3df8f9f33bf2a73f9cf0233bb9a95d58
techmsi <Sophie.Idromenos@nymag.com>
2015-11-12 10:03:20 -0500

    Site: Added prompts file

---
+++ generators/site/prompts.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var _ = require('lodash');
+
+function fixInputSlashes(input) {
+  // add/remove slashes if the input is more than just '/'
+  if (input === '/') {
+    return input;
+  }
+
+  if (!_.startsWith(input, '/')) {
+    // if it doesn't start with a slash, add one
+    input = '/' + input;
+  }
+
+  if (_.endsWith(input, '/')) {
+    // if it ends with a slash, remove it
+    input = input.substring(0, input.length - 1);
+  }
+
+  return input;
+};
+
+module.exports = function () {
+  console.log('in pro');
+  var prompts = [
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is the human-readable name of your site'
+    },
+    {
+      type: 'input',
+      name: 'host',
+      message: 'What is your site\'s domain name'
+    },
+    {
+      type: 'input',
+      name: 'path',
+      message: 'What is your site\'s path',
+      default: '/',
+      filter: fixInputSlashes
+    }
+  ];
+
+  return prompts;
+};
