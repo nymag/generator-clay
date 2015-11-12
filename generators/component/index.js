851cc3735997a38b95b05781090e04cfd98ca652
techmsi <Sophie.Idromenos@nymag.com>
2015-11-12 13:05:10 -0500

    Component: Simplified prompting logic

--- generators/component/index.js
+++ generators/component/index.js
@@ -60,39 +60,12 @@ module.exports = generators.NamedBase.extend({
 
   prompting: {
     getTplExtension: function () {
-      var done = this.async();
+      var done = this.async(),
+        prompts = require('./prompts.js')();
 
-      this.prompt([{
-        // ask for the template language
-        type: 'list',
-        name: 'templateLang',
-        message: 'What templating language do you want to use?',
-        default: 'nunjucks',
-        choices: [{
-          name: 'Nunjucks',
-          value: 'nunjucks'
-        }, {
-          name: 'Jade',
-          value: 'jade'
-        }, {
-          name: 'Other (enter on next prompt)',
-          value: 'other'
-        }],
-        store: true // store their defaults for the next time they use this
-      }, {
-        // ask for custom template language if they answered 'other'
-        type: 'input',
-        name: 'customTemplateLang',
-        message: 'Please type the extension you want',
-        filter: function (input) {
-          return input.indexOf('.') === 0 ? input.replace('.', '') : input;
-          // remove dot if they type it, e.g. .ejs -> ejs
-        },
-        when: function (answers) {
-          return answers.templateLang === 'other';
-        }
-      }], function (answers) {
-        this.tplExtension = answers.templateLang !== 'other' ? answers.templateLang : answers.customTemplateLang;
+      this.prompt(prompts, function (props) {
+        this.tplExtension = props.templateLang !== 'other' ? props.templateLang : props.customTemplateLang;
+
         done();
       }.bind(this));
     },
