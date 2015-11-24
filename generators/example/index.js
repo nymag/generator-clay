4cfaf38b196bb770301f141cb4776817f1c3ff01
techmsi <Sophie.Idromenos@nymag.com>
2015-11-24 10:55:42 -0500

    Example - Added placeholders for generators

---
+++ generators/example/index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var generators = require('yeoman-generator'),
+  chalk = require('chalk'),
+  example = {
+    appName: 'example',
+    componentName: 'hello-world',
+    siteName: 'test'
+  };
+
+module.exports = generators.Base.extend({
+  constructor: function () {
+    generators.Base.apply(this, arguments);
+    console.log(chalk.blue('You called the subgenerator for `' + chalk.bold.yellow('example') + '`.'));
+  },
+  writing: {
+    createInstance: function () {
+      this.log('Create instance: ', example.appName);
+    },
+    createComponent: function () {
+      this.log('Create component: ', example.componentName);
+    },
+    createSite: function () {
+      this.log('Create site: ', example.siteName);
+    }
+  }
+});
